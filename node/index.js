527aae2995287819d35d9ef765513cdd6cf26bf4
Michael Pierce <hi@mep.im>
2015-12-01 00:15:22 -0800

    index: js: cleanup

--- node/index.js
+++ node/index.js
@@ -6,21 +6,14 @@ var app = express();
 
 console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
-// APPROACH 1: Using environment variables created by Docker
-// var client = redis.createClient(
-// 	process.env.REDIS_PORT_6379_TCP_PORT,
-//   	process.env.REDIS_PORT_6379_TCP_ADDR
-// );
-
 // APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
 var client = redis.createClient('6379', 'redis');
 
-
+console.log('bo')
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
-    res.send('This page has been viewed ' + counter + ' times!');
-    console.log('hello');
+    res.send('This page has been viewedd ' + counter + ' times!');
   });
 });
 
