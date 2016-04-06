8a6f5fdc445b6640c56ac778d6c4d5b6e4028ff6
59023g <m.edward.pierce@gmail.com>
2016-04-05 18:08:16 -0700

    node/client: init

---
+++ node/client/client.js
@@ -0,0 +1,7 @@
+// 'use strict';
+// note - vendor deps
+var React    = require('react'),
+    ReactDOM = require('react-dom');
+
+
+// ReactDOM.render(React.createElement(inputForm), document.getElementById('input'));
