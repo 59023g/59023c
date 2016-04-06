fbab67bf1cf82c73e146c05895e4d6f4bbc75c02
59023g <m.edward.pierce@gmail.com>
2016-04-06 11:43:02 -0700

    .gitignore: redis/data

--- node/index.js
+++ node/index.js
@@ -15,7 +15,13 @@ var client = redis.createClient('6379', 'redis');
 
 app.use('/', express.static(__dirname + '/client/dist/' ));
 
-app.get('*', routes.index);
+app.get('/', function(req, res, next) {
+  client.incr('counter', function(err, counter) {
+    if(err) return next(err);
+    res.send('This page has been viewed ' + counter + ' times!');
+  });
+});
+
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
   console.log('Listening on port ' + (process.env.PORT || 8080));
