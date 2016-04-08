0edb2cb75bc34d20584185211511974c0173126d
59023g <m.edward.pierce@gmail.com>
2016-04-07 21:23:04 -0700

    node/server_index: html -> string

---
+++ node/server_index.js
@@ -0,0 +1,16 @@
+module.exports = (
+`
+<!doctype html public "storage">
+<html>
+  <head>
+    <meta charset=utf-8/>
+    <title>mep.im</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+  </head>
+  <body>
+    <div id=app></div>
+    <script src="/bundle.js"></script>
+  </body>
+</html>
+`
+)
