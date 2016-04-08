3d81e330dd89f8d43c7a06fc29b914ef3cb69d0d
59023g <m.edward.pierce@gmail.com>
2016-04-07 20:31:10 -0700

    node/client: rm shared.js, client.js

--- node/client/client.js
+++
@@ -1,7 +0,0 @@
-// 'use strict';
-// note - vendor deps
-var React    = require('react'),
-    ReactDOM = require('react-dom');
-
-
-// ReactDOM.render(React.createElement(inputForm), document.getElementById('input'));
