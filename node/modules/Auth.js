e84e01a4dbe189e28f90406bcb4dfcc2ecafd29f
59023g <m.edward.pierce@gmail.com>
2016-04-12 14:57:17 -0700

    node/Auth: init module.exports

---
+++ node/modules/Auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: {
+    id: 'mpierce',
+    fullName: 'Michael Pierce'
+  }
+
+}
--- node/modules/Meta.js
+++ node/modules/Meta.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import NavLink from './NavLink'
 
+
 export default React.createClass({
+  componentDidMount() {
+    console.log(this.props)
+  },
   render() {
     return (
       <div>
         <h2>Meta</h2>
         <ul>
-          <li><NavLink to="/:userName/meta/posts">Posts</NavLink></li>
+          <li><NavLink to={`/${this.props.params.userName}/meta/settings`}>Settings</NavLink></li>
         </ul>
         {this.props.children}
       </div>
