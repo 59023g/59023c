9e4e8fc12c4a8ae416d497b92874c3f241d44803
59023g <m.edward.pierce@gmail.com>
2016-04-11 23:31:20 -0700

    node/App: API Design

--- node/modules/App.js
+++ node/modules/App.js
@@ -2,6 +2,17 @@ import React from 'react'
 import NavLink from './NavLink'
 
 var user
+//https://github.com/reactjs/react-router/blob/master/examples/passing-props-to-children/app.js#L48
+// handler
+//http://stackoverflow.com/questions/34262526/react-router-passing-props-to-children
+//http://stackoverflow.com/questions/31862839/passing-props-to-react-router-children-routes
+
+// user 
+//   |
+//     -- meta
+//     -- posts
+//       |
+//         -- post
 
 export default React.createClass({
   componentDidMount() {
