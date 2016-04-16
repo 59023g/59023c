4529318ac0b150b429e8c5512772f237252e2104
59023g <m.edward.pierce@gmail.com>
2016-04-15 22:23:24 -0700

    node/posts: jsx <input>

--- node/modules/Posts.js
+++ node/modules/Posts.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import NavLink from './NavLink'
 
 export default React.createClass({
-  // getInitialState() { null },
+
+  propTypes: {
+    post: PropTypes.object
+  },
+
+
+  getInitialState() {
+    return {
+      user: null,
+      url: null,
+      title: null,
+      content: null,
+      abstract: null,
+      tags: [],
+      updated_at: null
+    }
+  },
   componentDidMount() {},
     contextTypes: {
       router: React.PropTypes.object
     },
 
-  createPost(event) {
-    event.preventDefault()
-    console.log(event)
+  createPost(e) {
+    e.preventDefault()
+    console.log(this.state)
 
     // const userName = event.target.elements[0].value
     // const postTitle = event.target.elements[1].value
     // const path = `/meta/posts/${userName}/${postTitle}`
     // this.context.router.push(path)
   },
-
+  handleChange(e) {
+    console.log(e.target.value)
+    var nextState = {};
+    nextState[e.target.name] = e.target.value;
+    console.log(nextState)
+    this.setState(nextState);
+  },
   render() {
     return (
       <div>
@@ -30,11 +52,33 @@ export default React.createClass({
               </li>
           ))}*/}
           <li>
-            <form onSubmit={this.createPost}>
-              <input type="text" placeholder="title" className="mb-12"/> {' '}
-              <textarea placeholder="content" className="mb-12 h-200">{}</textarea>
-              <textarea placeholder="abstract" className="mb-12">{}</textarea>
-              <input type="text" placeholder="tags" className="mb-12"/> {' '}
+            <form onSubmit={this.createPost} >
+              <input type="text"
+                placeholder="title"
+                className="mb-12"
+                name="title"
+                value={this.state.title}
+                onChange={this.handleChange}
+                />
+              <textarea placeholder="content"
+                className="mb-12 h-200"
+                name="content"
+                value={this.state.content}
+                onChange={this.handleChange}>
+              </textarea>
+              <textarea placeholder="abstract"
+                className="mb-12"
+                name="abstract"
+                value={this.state.abstract}
+                onChange={this.handleChange}>
+              </textarea>
+              <input type="text"
+                placeholder="tags"
+                className="mb-12"
+                name="tags"
+                value={this.state.tags}
+                onChange={this.handleChange}
+              />
               <button type="submit"><h3>POST</h3></button>
             </form>
           </li>
