72fd2161eaa5dc6418807daedc03a1e5917a20ae
59023g <hi@mep.im>
2016-05-02 22:06:11 -0700

    node/index: mv, refactor

--- node/index.js
+++ node/index.js
@@ -1,9 +1,23 @@
+
+
+// Redux - do not see use for it now
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import posts from './actions'
+//
+// let store = createStore(posts)
+
+// React App
 import React from 'react'
 import { render } from 'react-dom'
+
 import { Router, browserHistory } from 'react-router'
-import routes from './modules/routes'
+import App from './components/App'
+import routes from './components/routes'
 
-
-render((
-  <Router routes={routes} history={browserHistory}/>
-), document.getElementById('app'))
+render(
+    <Router routes={routes} history={browserHistory}>
+      <App />
+    </Router>,
+  document.getElementById('app')
+)
