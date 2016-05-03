d23d50c71902709d582e7b215f146db73ae7bd43
59023g <m.edward.pierce@gmail.com>
2016-05-02 23:58:50 -0700

    node/Home: add posts to home

--- node/components/Home.js
+++ node/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import Post from './Post'
+import { posts } from '../sweetData.js'
+
 export default React.createClass({
+  PropTypes: {
+    posts: React.PropTypes.array
+  },
   render() {
-    return <div>
-             Home
-           </div>
+    return (
+    <ul>
+      {posts.map(function (post, index) {
+         return (
+         <Post
+           user={post.user}
+           title={post.title}
+           content={post.content}
+           abstract={post.abstract}
+           tags={post.tags}
+           updated_at={post.updated_at} />)
+       })}
+    </ul>
+    )
   }
 })
