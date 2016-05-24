bf431c92535d842e70ef5972106ccf7e603f0365
59023g <m.edward.pierce@gmail.com>
2016-05-24 01:05:13 -0700

    node/server: rudimentary server

--- node/server.js
+++ node/server.js
@@ -9,33 +9,35 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { match, RouterContext } from 'react-router'
 
-import routes from './components/routes'
+// import routes from './components/routes'
 import index from './server_index.js'
 
 var app = express()
 app.use(compression())
 
-app.use(express.static(path.join(__dirname, 'client', 'public')))
+app.use(express.static(path.join(__dirname, 'lib', 'public')))
 
 
-console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
-
-var client = redis.createClient('6379', 'redis');
+// console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
+//
+// var client = redis.createClient('6379', 'redis');
 
 app.get('*', (req, res) => {
-  match({ routes, location: req.url }, (err, redirect, props) => {
-    if (err) {
-      res.status(500).send(err.message)
-    } else if (redirect) {
-      res.redirect(redirect.pathname + redirect.search)
-    } else if (props) {
-      const appHtml = renderToString(<RouterContext {...props}/>)
-      res.send(index)
-    } else {
-      res.status(404).send('Not Found')
-    }
-  })
-})
+
+  res.send(index)
+
+    // if (err) {
+    //   res.status(500).send(err.message)
+    // } else if (redirect) {
+    //   res.redirect(redirect.pathname + redirect.search)
+    // } else if (props) {
+    //   const appHtml = renderToString(<RouterContext {...props}/>)
+    //   res.send(index)
+    // } else {
+    //   res.status(404).send('Not Found')
+    // }
+  }
+)
 
 
 app.listen(process.env.PORT || 8080, function () {
