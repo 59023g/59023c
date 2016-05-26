2941fae93c1f457e891d607b42e0b58e1a6452b7
59023g <m.edward.pierce@gmail.com>
2016-05-26 01:01:31 -0700

    node/components: Application showMenu()

--- node/lib/components/Application.js
+++ node/lib/components/Application.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 import Menu from './Menu'
 import Footer from './Footer'
 import DisplayError from './DisplayError'
@@ -6,7 +7,8 @@ import DisplayError from './DisplayError'
 export default class Application extends React.Component {
 
   static propTypes = {
-    children: PropTypes.any
+    children: PropTypes.any,
+    application: PropTypes.object.isRequired
   };
 
   constructor (props, context) {
@@ -25,22 +27,28 @@ export default class Application extends React.Component {
   }
 
   render () {
+
+    const loggedIn = Boolean(this.props.application.token)
+
     const { isMenuActive } = this.state
     const activeClass = isMenuActive ? 'active' : ''
 
+    const showMenu = function() {
+      if(loggedIn) {
+        return (
+          <Menu activeClass={activeClass} />
+        )
+      }
+    }
+
+
     return (
       <div id="layout" className={activeClass}>
-        <a href="#menu" id="menuLink"
-          className={'menu-link'}
-          onClick={this.handleMenuClick}>
-          <span></span>
-        </a>
 
-        <Menu activeClass={activeClass} />
+        {showMenu()}
 
         <div id="main">
           <DisplayError />
-          {/* this will render the child routes */}
           {this.props.children}
         </div>
 
@@ -49,3 +57,7 @@ export default class Application extends React.Component {
     )
   }
 }
+
+export default connect(
+  ({ application }) => ({ application })
+)(Application)
