0b5344dfc9e68f808b5296c449bc9d8914ad4972
59023g <m.edward.pierce@gmail.com>
2016-06-01 21:55:28 -0700

    utils/handle-error: error mapping

--- node/lib/utils/handle-action-error.js
+++ node/lib/utils/handle-action-error.js
@@ -1,9 +1,10 @@
 import { SHOW_ERROR } from '../constants'
 
 export default function handleActionError (dispatch, error, source) {
+  console.log('action', error)
   return dispatch({
     type: SHOW_ERROR,
     source,
-    payload: error
+    payload: error.response
   })
 }
