4e697fd44b52d9c43cd33f0e9a1731abff85f408
59023g <m.edward.pierce@gmail.com>
2016-06-02 16:37:27 -0700

    reducers/posts: initialState, actionHandlers

---
+++ node/lib/reducers/posts.js
@@ -0,0 +1,25 @@
+import * as constants from '../constants'
+import createReducer from '../utils/create-reducer'
+
+const initialState = {
+  user: {},
+  repo: {},
+  posts: {
+    user: [],
+    repo: [],
+    pagination: {}
+  }
+}
+
+const actionHandlers = {
+  [constants.FETCH_USER]: (state, action) => ({ user: action.user }),
+  [constants.FETCH_USER_POSTS]: (state, action) => (
+    {
+      posts: Object.assign({}, state.posts, {
+        user: action.posts,
+        pagination: action.pagination
+      })
+    })
+}
+
+export default createReducer(initialState, actionHandlers)
