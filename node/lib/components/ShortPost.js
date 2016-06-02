71d670a7ca4ce55329d75b1e88565f835d837fdb
59023g <m.edward.pierce@gmail.com>
2016-06-02 16:48:38 -0700

    comp/Post, Shortpost: split posts up homepage and post page

--- node/lib/components/ShortPost.js
+++ node/lib/components/ShortPost.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router'
 export default React.createClass({
   propTypes: {
     user: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     abstract: PropTypes.string,
@@ -17,7 +18,7 @@ export default React.createClass({
   render: function () {
     return (
     <li>
-      <Link to={this.props.url}>
+      <Link to={this.props.username + '/' + this.props.url}>
         <h2 dangerouslySetInnerHTML={this.rawMarkup(this.props.title)}></h2>
       </Link>
       <p dangerouslySetInnerHTML={this.rawMarkup(this.props.content)}></p>
--- node/lib/components/pages/Post.js
+++ node/lib/components/pages/Post.js
@@ -1,7 +1,14 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 
-export default React.createClass({
-  propTypes: {
+
+export default class Post extends React.Component {
+
+  constructor (props, context) {
+    super(props, context)
+  }
+
+  static propTypes = {
     user: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
@@ -9,13 +16,16 @@ export default React.createClass({
     tags: PropTypes.array,
     updatedAt: PropTypes.number.isRequired,
     url: PropTypes.string.isRequired
-  },
-  rawMarkup: function (value) {
+  }
+
+  rawMarkup (value) {
     return { __html: value }
-  },
-  render: function () {
+  }
+
+  render () {
     return (
     <li>
+      <span>test</span>
       <h2 dangerouslySetInnerHTML={this.rawMarkup(this.props.title)}></h2>
       <p dangerouslySetInnerHTML={this.rawMarkup(this.props.content)}></p>
       <p dangerouslySetInnerHTML={this.rawMarkup(this.props.abstract)}></p>
@@ -24,4 +34,8 @@ export default React.createClass({
     </li>
     )
   }
-})
+}
+
+export default connect(
+  ({ application }) => ({ application })
+)(Post)
