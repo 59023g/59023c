f408329e985b4c191af32bae8a84a27646bee640
59023g <m.edward.pierce@gmail.com>
2016-06-09 10:38:21 -0700

    reducers/application: actionHandler LOGGED_IN

--- node/lib/reducers/application.js
+++ node/lib/reducers/application.js
@@ -16,9 +16,16 @@ const initialState = {
 }
 
 const actionHandlers = {
-  [constants.LOGGED_IN]: (_, action) => {
+  [constants.LOGGED_IN]: (state, action) => {
+    const { payload, source } = action
+    return Object.assign({}, state, {
+      token: payload.token,
+      username: payload.user,
+      user: {
+        permission: ["all"]
+      }
+    })
 
-    console.log(action.payload)
   },
   [constants.LOG_OUT]: () => ({ token: null }),
   [constants.LOCALE_SWITCHED]: (_, action) => ({ locale: action.payload }),
@@ -27,6 +34,7 @@ const actionHandlers = {
   // We should also handle all other kind of application errors,
   // report them and show some kind of helpful message to the user.
   [constants.SHOW_ERROR]: (state, action) => {
+    console.log('show error', action)
     const { payload, source } = action
     return Object.assign({}, state, {
       // TODO: ideally we want to map API error response codes
@@ -34,7 +42,7 @@ const actionHandlers = {
       error: {
         source,
         message: payload.message || payload.statusText,
-        url: payload.url,
+        url: payload.url || null,
         statusCode: payload.statusCode || payload.code || payload.status,
         body: payload.body || (payload instanceof Error ?
           (payload.toString() + '\n' + payload.stack) : payload)
