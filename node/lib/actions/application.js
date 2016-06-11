a754d4428c7b17b4a0dc1042ca71667169f81f96
59023g <m.edward.pierce@gmail.com>
2016-06-11 12:10:25 -0700

    eslint --fix

--- node/lib/actions/application.js
+++ node/lib/actions/application.js
@@ -15,20 +15,23 @@ import handleActionError from '../utils/handle-action-error'
 
 const USER_API = '/user.json'
 
-export function login(form) {
+export function login (form, redirect) {
   return function (dispatch) {
     dispatch(requestLogin(form))
     return fetch(USER_API)
     .then(response => {
-        if (!response.ok) {
-          throw {
-            response
-          }
+      if (!response.ok) {
+        throw {
+          response
         }
-        return response.json()
-      })
+      }
+      return response.json()
+    })
       .then(json => {
         dispatch(receiveLogin(json))
+      })
+      .then( () => {
+        if (redirect) redirect()
       })
       .catch((error) => {
         handleActionError(dispatch, error, REQUEST_LOGIN)
@@ -37,28 +40,28 @@ export function login(form) {
 }
 
 
-export function requestLogin(form) {
+export function requestLogin (form) {
   return {
     type: REQUEST_LOGIN,
     form
   }
 }
 
-export function receiveLogin(payload) {
+export function receiveLogin (payload) {
   return {
     type: RECEIVE_LOGIN,
     payload
   }
 }
 
-export function switchLocale(locale) {
+export function switchLocale (locale) {
   return {
     type: LOCALE_SWITCHED,
     locale
   }
 }
 
-export function hideError() {
+export function hideError () {
   return {
     type: HIDE_ERROR
   }
--- node/lib/reducers/application.js
+++ node/lib/reducers/application.js
@@ -24,35 +24,35 @@ export default function (state, action) {
   if (typeof state === 'undefined')
     return initialState
   switch (action.type) {
-  case REQUEST_LOGIN:
-    return Object.assign({}, state, {
-      ...state,
-      isFetching: true
-    })
-  case RECEIVE_LOGIN:
-    return Object.assign({}, state, {
-      isFetching: false,
-      ...action.payload
-    })
-  case LOG_OUT:
-    return initialState
-  case SHOW_ERROR:
-    return Object.assign({}, state, {
-      error: {
-        action,
-        message: action.message || action.statusText,
-        url: action.message || null,
-        statusCode: action.statusCode || action.code || action.status,
-        body: action.body || (action instanceof Error ?
+    case REQUEST_LOGIN:
+      return Object.assign({}, state, {
+        ...state,
+        isFetching: true
+      })
+    case RECEIVE_LOGIN:
+      return Object.assign({}, state, {
+        isFetching: false,
+        ...action.payload
+      })
+    case LOG_OUT:
+      return initialState
+    case SHOW_ERROR:
+      return Object.assign({}, state, {
+        error: {
+          action,
+          message: action.message || action.statusText,
+          url: action.message || null,
+          statusCode: action.statusCode || action.code || action.status,
+          body: action.body || (action instanceof Error ?
           (action.toString() + '\n' + action.stack) : action)
-      }
-    })
-  case HIDE_ERROR:
-    return Object.assign({}, state, {
-      ...state,
-      error: null
-    })
-  default:
-    return state
+        }
+      })
+    case HIDE_ERROR:
+      return Object.assign({}, state, {
+        ...state,
+        error: null
+      })
+    default:
+      return state
   }
 }
