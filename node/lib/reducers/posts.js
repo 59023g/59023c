bca0a71d3988bd4f282fa39eb56df0f8b1004f20
59023g <m.edward.pierce@gmail.com>
2016-06-11 23:28:20 -0700

    reducers/posts: REQUEST_POSTS, RECEIVE_POSTS

--- node/lib/reducers/posts.js
+++ node/lib/reducers/posts.js
@@ -1,25 +1,46 @@
-import * as constants from '../constants'
-import createReducer from '../utils/createReducer'
+import {
+  REQUEST_POSTS,
+  RECEIVE_POSTS,
+  // FETCH_USER,
+  // FETCH_REPO,
+  // FETCH_USER_STARGAZERS,
+  // FETCH_REPO_STARGAZERS
+} from '../constants'
 
 const initialState = {
-  user: {},
-  repo: {},
-  posts: {
-    user: [],
-    repo: [],
-    pagination: {}
+  isFetching: false,
+  didInvalidate: false,
+  receivedAt: null,
+  posts: []
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_POSTS:
+      return Object.assign({}, state, {
+        ...state,
+        isFetching: true
+      })
+    case RECEIVE_POSTS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        receivedAt: action.receivedAt,
+        posts: action.payload
+      })
+    default:
+      return state
   }
 }
 
-const actionHandlers = {
-  [constants.FETCH_USER]: (state, action) => ({ user: action.user }),
-  [constants.FETCH_USER_POSTS]: (state, action) => (
-    {
-      posts: Object.assign({}, state.posts, {
-        user: action.posts,
-        pagination: action.pagination
-      })
-    })
-}
-
-export default createReducer(initialState, actionHandlers)
+// const actionHandlers = {
+//   [constants.FETCH_USER]: (state, action) => ({ user: action.user }),
+//   [constants.FETCH_USER_POSTS]: (state, action) => (
+//     {
+//       posts: Object.assign({}, state.posts, {
+//         user: action.posts,
+//         pagination: action.pagination
+//       })
+//     })
+// }
+//
+// export default createReducer(initialState, actionHandlers)
