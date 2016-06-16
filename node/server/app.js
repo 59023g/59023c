849ee00e701b0928d7038a5731e33e2a3b4a053c
59023g <m.edward.pierce@gmail.com>
2016-06-15 21:47:20 -0700

    server/app: in transition

--- node/server/app.js
+++ node/server/app.js
@@ -1,25 +1,71 @@
-var express = require('express')
-var path = require('path')
-var compression = require('compression')
+import express from 'express'
+import path from 'path'
+import compression from 'compression'
 
-var redis = require('redis')
+import redis from 'redis'
 
-var React = require('react')
-// var { renderToString } = require('react-dom/server')
-// var { match, RouterContext } = require('react-router')
+import React from 'react'
+import ReactDomServer from 'react-dom/server'
+
+import configureStore from '../shared/store/configureStore'
+
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+import  index from '../lib/index'
+
+console.log(index)
+console.log(configureStore)
+// import { store } from '../lib/Root'
+
+// import HomePage from '../lib/containers/HomePage'
+
+// var { match, RouterContext } require('react-router')
 
 //var routes = require('./components/routes'
-var index = require('../server_index.js')
+// import index from './server_index.js'
 
-var app = express()
 
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
-var publicPath = path.resolve(__dirname, 'public');
+var publicPath = path.join(__dirname, '../static');
 
+var app = express()
+app.use(express.static(publicPath));
 app.use(compression())
 
-app.use(express.static(publicPath))
+var initialState = {
+    application: {
+      token: null,
+      locale: 'en',
+      user: { permissions: [] }
+    },
+    posts: null,
+    router: null
+}
+
+var stateJSON = JSON.stringify(state).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--');
+// return {
+//   initialState: "window.__INITIAL_STATE__ = "+stateJSON
+// }
+
+/* Using this.props.initialState in the Index component */
+var Index = React.createClass({
+  render: function() {
+    return (
+      <html>
+        <head>
+          /* ... */
+          <script dangerouslySetInnerHTML={{__html: this.props.initialState}} />
+        </head>
+	/* ... */
+      </html>
+    )
+  }
+});
+
+
+
+var store = store.configureStore(reducers, initialState)
 
 app.get('/api/posts', function (req, res) {
   console.log(req.query)
@@ -27,24 +73,17 @@ app.get('/api/posts', function (req, res) {
   //send(res, api.queryPosts(query))
 })
 
-app.get('/', (req, res) => {
-  res.send(index)
+app.get('*', (req, res) => {
+  res.send("<!DOCTYPE html>"+
+  ReactDOMServer.renderToString(
+    Provider({store: store}, RouterContext(renderProps))
+  )
+);
 })
 
 // console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 //
 // var client = redis.createClient('6379', 'redis');
-
-// app.get('*', (req, res) => {
-//   console.log(req.url)
-//   res.send(index)
-// })
-//
-// app.get('/mock_api/user.json', (req, res) => {
-//   console.log(req.url)
-//   res.send('hello')
-// })
-
 
 app.listen(port, function() {
     console.log(' Listening on port: ' +  port );
