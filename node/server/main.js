4f36800a282bedc0b011bb19dccea2ca9ab6b06d
59023g <m.edward.pierce@gmail.com>
2016-06-18 12:14:42 -0700

    server/main: abstract 'app'

--- node/server/main.js
+++ node/server/main.js
@@ -1,1 +1,9 @@
+import http from 'http'
+import nconf from 'nconf'
+
 const app = require('./app');
+let port = 3001
+let server = http.createServer(app)
+server.listen(port)
+
+console.log('express server listening on ' + port )
