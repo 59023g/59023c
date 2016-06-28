684e3533d9cca4c911f57d003accebbfdc0694e3
59023g <m.edward.pierce@gmail.com>
2016-06-27 19:05:09 -0700

    {client, server}/api: clean up

--- node/client/impl/api.js
+++ node/client/impl/api.js
@@ -1,3 +1,14 @@
-export default function whichApi () {
+const POSTS_API = '/api/posts'
+
+function whichApi () {
   console.log('client api')
+}
+
+function queryPosts() {
+  return fetch(POSTS_API)
+}
+
+module.exports = {
+  whichApi,
+  queryPosts
 }
--- node/server/impl/api.js
+++ node/server/impl/api.js
@@ -1,3 +1,25 @@
-export function whichApi () {
+import fetch from 'node-fetch'
+
+const POSTS_API = 'http://localhost:3001/api/posts'
+
+function whichApi () {
   console.log('server api')
+}
+
+function queryPosts() {
+  console.log('server queryPosts')
+  // return posts
+  return fetch(POSTS_API)
+
+  // .then(res => {
+  //   console.log('res', res)
+  // })
+    // .then(res => console.log('server', res))
+}
+
+
+module.exports = {
+  whichApi,
+  queryPosts
+
 }
