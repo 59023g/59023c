ce284331d4113cf520ec49e23110553cbe35a834
59023g <m.edward.pierce@gmail.com>
2016-06-27 19:04:13 -0700

    utils/fetchComponentDataBeforeRender: error handling for pseudo routes

--- node/shared/utils/fetchComponentDataBeforeRender.js
+++ node/shared/utils/fetchComponentDataBeforeRender.js
@@ -1,15 +1,16 @@
-/**
-* This looks at static needs parameter in components and waits for the promise to be fullfilled
-* It is used to make sure server side rendered pages wait for APIs to resolve before returning res.end()
-https://github.com/caljrimmer/isomorphic-redux-app
-*/
 
 export function fetchComponentDataBeforeRender(dispatch, components, params) {
+
   const needs = components.reduce( (prev, current) => {
+
+    if (!current)
+      return []
+
     return (current.need || [])
       .concat((current.WrappedComponent ? current.WrappedComponent.need : []) || [])
       .concat(prev);
     }, []);
+
     const promises = needs.map(need => dispatch(need()));
     return Promise.all(promises);
 }
