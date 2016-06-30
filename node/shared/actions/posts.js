ca46fb0d26b41781cc7d28ca30f134d33fc7134b
59023g <m.edward.pierce@gmail.com>
2016-06-29 20:28:27 -0700

    actions/post: getPost(id)

--- node/shared/actions/posts.js
+++ node/shared/actions/posts.js
@@ -13,6 +13,38 @@ import {
   api.whichApi()
 })()
 
+export function getPost(id) {
+  return function (dispatch, getState) {
+
+    if (typeof id !== 'number')
+      id = parseInt(id)
+
+    const state = getState()
+    const posts = state.posts.posts
+
+    if (posts.length > 0) {
+      const post = posts.find(post => {
+        return post.id === id
+      });
+      return post
+    } else {
+
+    }
+
+
+    // if(!post) {
+    //   return dispatch({
+    //     type: constants.FETCH_POST,
+    //     id: id,
+    //     [fields.PROMISE]: api.getPost(id)
+    //   });
+    // }
+    // else {
+    //   return post;
+    // }
+  };
+}
+
 function requestPosts () {
   return {
     type:  REQUEST_POSTS
@@ -47,6 +79,7 @@ export function fetchPostsIfNeeded() {
   }
 }
 
+// todo - query parameters
 function fetchPosts () {
   return function (dispatch) {
     dispatch(requestPosts())
