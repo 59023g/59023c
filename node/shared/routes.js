95ff88ae3a6f2f92e5b6063e60e37b0ab70ee68a
59023g <m.edward.pierce@gmail.com>
2016-07-15 15:52:19 -0700

    shared/routes: PostPage add :id, abstract logout to application.logout

--- node/shared/routes.js
+++ node/shared/routes.js
@@ -2,29 +2,46 @@ import React from 'react'
 import { Router, Route, Redirect } from 'react-router'
 import * as components from '../shared/components'
 
-import { requireAuth, logout } from './auth'
+import { getPost } from './actions/posts'
 
+
+// import { requireAuth, logout } from './containers/Application'
 const {
   MetaHome,
   Application,
   HomePage,
-  Post,
+  PostPage,
   Login,
+  Logout,
   SuperSecretArea
 } = components
 
+function requireAccess () {
+  // console.log(Application.requireAuth)
+  // return requireAuth()
+}
+
+function load(nextState) {
+
+  if (nextState.params.id) {
+    getPost(nextState.params.id).then(data => {
+      console.log(data)
+    })
+  }
+}
+
 export default function () {
   return (
-      <Route component={Application}>
-        <Route path="/" component={HomePage} />
-        <Route path=":username/:date/:postTitle" component={Post} />
-        <Route path="meta" component={MetaHome} onEnter={requireAuth}>
-          <Route path="secret-area" component={SuperSecretArea} />
+      <Route component={ Application }>
+        <Route path="/" component={ HomePage } />
+        <Route path="/:username/:date/:postTitle/:id" component={ PostPage } onEnter={ load }/>
+        <Route path="meta" component={ MetaHome } onEnter={ requireAccess }>
+          <Route path="secret-area" component={ SuperSecretArea } />
         </Route>
         <Redirect from="/login" to="/meta/login" />
 
-        <Route path="/meta/login" component={Login} />
-        <Route path="logout" onEnter={logout} />
+        <Route path="/meta/login" component={ Login } />
+        <Route path="logout" onEnter={ Application.logout } />
       </Route>
   )
 }
