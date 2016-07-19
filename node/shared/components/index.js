0e67ea78d3c60ce6ed3e8a64b9e1be87c3ab64a1
59023g <m.edward.pierce@gmail.com>
2016-07-19 11:27:53 -0700

    components/index: rm github

--- node/shared/components/index.js
+++ node/shared/components/index.js
@@ -5,10 +5,6 @@ export { default as MetaHome } from '../containers/MetaHome'
 export { default as PostPage } from '../containers/PostPage'
 
 export { default as Forbidden } from '../containers/Forbidden'
-export { default as GithubStargazers } from '../containers/GithubStargazers'
 export { default as Login } from '../containers/Login'
 export { default as HomePage } from '../containers/HomePage'
 export { default as SuperSecretArea } from '../containers/SuperSecretArea'
-
-export { default as GithubRepo } from './github/Repo'
-export { default as GithubUser } from './github/User'
