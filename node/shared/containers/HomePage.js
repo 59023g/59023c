894229589bed2719b3c580f466a0a03aab4432f4
59023g <m.edward.pierce@gmail.com>
2016-07-18 18:54:57 -0700

    containers/HomePage: refactor

--- node/shared/containers/HomePage.js
+++ node/shared/containers/HomePage.js
@@ -5,26 +5,13 @@ import { connect } from 'react-redux'
 import { defineMessages, FormattedMessage } from 'react-intl'
 
 import ShortPost from '../components/ShortPost'
-import { fetchPostsIfNeeded } from '../actions/posts'
-
-
-const messages = defineMessages({
-  welcome: {
-    id: 'home.welcome',
-    description: 'welcome',
-    defaultMessage: 'welcom'
-  },
-  intro: {
-    id: 'home.intro',
-    description: 'texttt',
-    defaultMessage: 'text'
-  }
-})
+import { fetchPosts } from '../actions/posts'
 
 
 function loadData(props) {
-
+  props.fetchPosts()
 }
+
 export default class HomePage extends React.Component {
 
   constructor (props) {
@@ -32,7 +19,7 @@ export default class HomePage extends React.Component {
   }
 
   componentWillMount () {
-    this.props.fetchPostsIfNeeded()
+    this.props.loadData()
   }
 
   render () {
@@ -42,35 +29,37 @@ export default class HomePage extends React.Component {
     }
     const posts = this.props.posts.posts;
 
-    return (
-      <div>
-        <div className="content">
-          <ul>
-            { posts.map(function (post, index) {
-              return (
-               <ShortPost
-                 key={index}
-                 user={post.user}
-                 username={post.author.username}
-                 title={post.title}
-                 content={post.content}
-                 abstract={post.abstract}
-                 tags={post.tags}
-                 updatedAt={post.updatedAt}
-                 url={post.url}
-                 id={post.id}/>)
-            })}
-          </ul>
-        </div>
-      </div>
-    )
+    console.log('posts', this.props.posts)
+    // return (
+    //   <div>
+    //     <div className="content">
+    //       <ul>
+    //         { posts.map(function (post, index) {
+    //           return (
+    //            <ShortPost
+    //              key={index}
+    //              user={post.user}
+    //              username={post.author.username}
+    //              title={post.title}
+    //              content={post.content}
+    //              abstract={post.abstract}
+    //              tags={post.tags}
+    //              updatedAt={post.updatedAt}
+    //              url={post.url}
+    //              id={post.id}/>)
+    //         })}
+    //       </ul>
+    //     </div>
+    //   </div>
+    // )
   }
 }
 
 
 HomePage.propTypes = {
-  fetchPostsIfNeeded: PropTypes.func.isRequired,
+  // fetchPostsIfNeeded: PropTypes.func.isRequired,
   // selectedPosts: PropTypes.array.isRequired,
+  fetchPosts: PropTypes.func.isRequired,
   posts: PropTypes.object.isRequired
 }
 
@@ -81,16 +70,15 @@ HomePage.contextTypes = {
 
 function mapStateToProps(state, ownProps) {
   return {
-
+    posts: state.posts
   }
 }
 // note - server side render waits until this call returns -
 // see fetchComponentDataBeforeRender()
 HomePage.need = [
-  fetchPostsIfNeeded
+  // fetchPostsIfNeeded
+  fetchPosts
 ]
 
-export default connect(
-  ({ application, posts }) => ({ application, posts }),
-  { fetchPostsIfNeeded }
+export default connect(mapStateToProps, { fetchPosts }
 )(HomePage)
