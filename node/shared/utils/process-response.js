fab322dba00d723471ce29dc8fe4951c12860cf9
59023g <m.edward.pierce@gmail.com>
2016-07-24 11:00:29 -0700

    utils/proces-response: Schemas, normalize

--- node/shared/utils/process-response.js
+++ node/shared/utils/process-response.js
@@ -1,41 +1,37 @@
 import { Schema, arrayOf, normalize } from 'normalizr'
 
+const userSchema = new Schema('authors', {
+  idAttribute: 'id'
+})
+
 const postSchema = new Schema('posts', {
   idAttribute: 'id'
 })
 
-const postAuthorSchema = new Schema('postAuthors', {
-  idAttribute: 'id'
-})
-
-
-
 postSchema.define({
-  author: postAuthorSchema
+  author: userSchema
 })
 
-// Schemas for Github API responses.
-// export const Schemas = {
-//   USER: postAuthorSchema,
-//   USER_ARRAY: arrayOf(postAuthorSchema),
-//   POST: postSchema,
-//   POST_ARRAY: arrayOf(postSchema)
-// }
+export const Schemas = {
+  USER: userSchema,
+  USER_ARRAY: arrayOf(userSchema),
+  POST: postSchema,
+  POST_ARRAY: arrayOf(postSchema)
+}
 
-export default function processResponse (response) {
-  let isOk = response.ok
-  console.log('normalbody?', normalize(response.body, postSchema))
 
-  return response.text()
+export function processResponse (response, schema) {
+  console.log('response', response, 'schema,', schema)
+  return response.json()
+  .then(json => ({ json, response }))
+  .then(({ json, response }) => {
+    if (!response.ok) {
+      return Promise.reject(json)
+    }
+    console.log('json', json, 'response', response)
 
-  .then(body => {
-    try { body = JSON.parse(body) }
-    catch (error) { if (isOk) isOk = false }
-
-    console.log('normal?', normalize(body, postSchema))
-    console.log('body', body)
-    if (isOk) return normalize(body, postSchema)
-
-    throw { ...body, statusCode: response.status }
+    return Object.assign({},
+      normalize(json, schema), {}
+    )
   })
 }
