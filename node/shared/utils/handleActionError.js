4141cadc550cef72de2f47976e3f738a805e028a
59023g <m.edward.pierce@gmail.com>
2016-07-24 11:00:49 -0700

    utils/handleActionError: more detail

--- node/shared/utils/handleActionError.js
+++ node/shared/utils/handleActionError.js
@@ -1,9 +1,10 @@
 import { SHOW_ERROR } from '../constants'
 
 export default function handleActionError (dispatch, error, source) {
+  console.log('error', error)
   return dispatch({
     type: SHOW_ERROR,
     source,
-    payload: error.response
+    payload: error
   })
 }
